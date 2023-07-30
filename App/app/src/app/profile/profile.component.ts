ea4f7372b2274208d68597598023a6d7cbea4b19
dtsankov <dimitur.tsankov@gmail.com>
2023-07-30 23:52:39 +0300

    Implements nav styling and profile details and edit functionality

--- App/app/src/app/profile/profile.component.ts
+++ App/app/src/app/profile/profile.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { AuthService } from '../auth/auth.services';
 import { IAdventure } from '../interfaces/adventure';
+import { FormBuilder, Validators } from '@angular/forms';
+import { DEFAULT_EMAIL_DOMAINS } from '../shared/constants';
+import { appEmailValidator } from '../shared/validators/app-email-validator';
+
+interface Profile {
+  username : string;
+  email:string;
+  img:string;
+}
 
 @Component({
   selector: 'app-profile',
@@ -11,14 +20,37 @@ import { IAdventure } from '../interfaces/adventure';
 export class ProfileComponent implements OnInit {
   adventureList: IAdventure[] | null = null;
 
+  isEditMode: boolean = false;
+
+  profileDetails: Profile = {
+    username: this.user.username,
+    email: this.user.email,
+    img: this.user.img
+     
+  };
+
+
+  form = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    email: [
+      '',
+      [Validators.required, appEmailValidator(DEFAULT_EMAIL_DOMAINS)],
+    ],
+    tel: [''],
+  });
+ 
+
+
   constructor(
     private apiServices: ApiService,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb : FormBuilder
   ) {}
 
   get user() {
-    const { email, img } = this.authService.user!;
+    const {username,email, img } = this.authService.user!;
     return {
+      username,
       email,
       img,
     };
@@ -33,5 +65,19 @@ export class ProfileComponent implements OnInit {
         console.error(err);
       },
     });
+  }
+
+  toggleEditMode(): void {
+    this.isEditMode = !this.isEditMode;
+  }
+  saveProfileHandler(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.profileDetails = { ...this.form.value } as Profile;
+    console.log(this.form.value);
+
+    this.toggleEditMode();
   }
 }
